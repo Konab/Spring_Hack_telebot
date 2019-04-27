5201585c6e5f6694abbf74511929734a476276cf
Konab <konab-job@yandex.ru>
2019-04-27 23:45:09 +0300

    add current btn

--- main.py
+++ main.py
@@ -3,17 +3,21 @@ from config import Config
 from telebot import types
 
 
+Keyboards = {'get_help': '💁‍♀️ Справочная информация',
+			'get_phone': '☎️ Телефон оператора'
+		}
+
 if __name__ == '__main__':
 	bot = TeleBot(Config.TOKEN)
 
-	def send_menu_col():
+	def send_menu_col(chat_id):
 		# It defines how many button are fit on each row before continuing on next row
 		markup = types.ReplyKeyboardMarkup(row_width=2)
-		itembtn1 = types.KeyboardButton('a')
-		itembtn2 = types.KeyboardButton('v')
-		itembtn3 = types.KeyboardButton('d')
-		markup.add(itembtn1, itembtn2, itembtn3)
-		bot.send_message(241612123, 'Choose one letter:', reply_markup=markup)
+		for key in Keyboards:
+			markup.row(Keyboards[key])
+
+		markup.add()
+		bot.send_message(chat_id, 'Choose one letter:', reply_markup=markup)
 
 	def send_menu_row():
 		# or add KeyboardButton one row at a time:
@@ -32,7 +36,7 @@ if __name__ == '__main__':
 	def start_handler(messege):
 		print('::> Start by user: {}, id: {}'.format(messege.from_user.first_name, messege.chat.id))
 		bot.send_message(messege.chat.id, 'Hello, *{}*'.format(messege.from_user.first_name), parse_mode='markdown')
-		send_menu_col()
+		send_menu_col(messege.chat.id)
 
 
 	@bot.message_handler(commands=['info'])
