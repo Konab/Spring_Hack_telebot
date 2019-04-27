17e7e618252f17ebf3e36fc977699f10c7b0dbf8
Konab <konab-job@yandex.ru>
2019-04-27 22:11:44 +0300

    add markup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from telebot import TeleBot
 from config import Config
+from telebot import types
 
 
 if __name__ == '__main__':
@@ -10,6 +11,15 @@ if __name__ == '__main__':
 		print('::> Start by user: {}, id: {}'.format(messege.from_user.first_name, messege.chat.id))
 		bot.send_message(messege.chat.id, 'Hello, *{}*'.format(messege.from_user.first_name), parse_mode='markdown')
 
+	def send_menu():
+		markup = types.ReplyKeyboardMarkup(row_width=2)
+		itembtn1 = types.KeyboardButton('a')
+		itembtn2 = types.KeyboardButton('v')
+		itembtn3 = types.KeyboardButton('d')
+		markup.add(itembtn1, itembtn2, itembtn3)
+		bot.send_message(241612123, 'Choose one letter:', reply_markup=markup)
+
 
 	print('--> Запускаю бота')
 	bot.polling(True)
+	send_menu()
