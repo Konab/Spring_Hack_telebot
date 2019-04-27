9b674653bdea7f38e44e116c60956382291c6452
Konab <konab-job@yandex.ru>
2019-04-27 20:13:20 +0300

    dix

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from telebot import TeleBot
 from config import Config
 
 
-@bot.message_handler(commands=['start'])
-def start_handler(messege):
-	print('::> Start by user: {}, id: {}'.format(messege.from_user.first_name, messege.chat.id))
-	bot.send_message(messege.chat.id, 'Hello, *{}*'.format(messege.from_user.first_name), parse_mode='markdown')
-
-
 if __name__ == '__main__':
+
+	@bot.message_handler(commands=['start'])
+	def start_handler(messege):
+		print('::> Start by user: {}, id: {}'.format(messege.from_user.first_name, messege.chat.id))
+		bot.send_message(messege.chat.id, 'Hello, *{}*'.format(messege.from_user.first_name), parse_mode='markdown')
+
+
 	bot = TeleBot(Config.TOKEN)
 	print('--> Запускаю бота')
 	bot.polling(True)
